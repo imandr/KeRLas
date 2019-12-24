11892ac5b462ac98fb2d7ce4f611e14ae5d9629e
Igor Mandrichenko <igorvm@gmail.com>
2019-12-24 13:02:00 -0600

    Lander works

--- AC-Phil/ac.py
+++ AC-Phil/ac.py
@@ -1,5 +1,5 @@
 from keras import backend as K
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
 import numpy as np
@@ -21,10 +21,18 @@ class Agent(object):
     def build_actor_critic_network(self):
         input = Input(shape=(self.input_dims,))
         delta = Input(shape=[1])
+        tau = Input(shape=[1])
         dense1 = Dense(self.fc1_dims, activation='relu')(input)
         dense2 = Dense(self.fc2_dims, activation='relu')(dense1)
-        probs = Dense(self.n_actions, activation='softmax')(dense2)
         values = Dense(1, activation='linear')(dense2)
+
+        pre_probs = Dense(self.n_actions, activation='linear')(dense2)
+        
+        def tau_softmax(args):
+            pp, t = args
+            return K.softmax(pp*t)
+        
+        probs = Lambda(tau_softmax)([pre_probs, tau])
 
         def custom_loss(y_true, y_pred):
             out = K.clip(y_pred, 1e-8, 1-1e-8)
@@ -32,26 +40,29 @@ class Agent(object):
 
             return K.sum(-log_lik*delta)
             
-        actor = Model(inputs=[input, delta], outputs=[probs])
+        policy = Model(inputs=[input, tau], outputs=[probs])
+        
+        actor = Model(inputs=[input, tau, delta], outputs=[probs])
         actor.compile(optimizer=Adam(lr=self.alpha), loss=custom_loss)
         
         critic = Model(inputs=[input], outputs=[values])
         critic.compile(optimizer=Adam(lr=self.beta), loss="mse")
         
-        policy = Model(inputs=[input], outputs=[probs])
         
         return actor, critic, policy
 
-    def choose_action(self, observation):
+    def choose_action(self, observation, tau=1.0):
         state = observation[np.newaxis, :]
-        probabilities = self.policy.predict(state)[0]
+        tau = np.ones((len(observation),))*tau
+        probabilities = self.policy.predict([state, tau])[0]
         action = np.random.choice(self.action_space, p=probabilities)
 
         return action
 
-    def learn(self, state, action, reward, state_, done):
+    def learn(self, state, action, reward, state_, done, tau=1.0):
         state = state[np.newaxis,:]
         state_ = state_[np.newaxis,:]
+        tau = np.ones((len(state),))*tau
         critic_value_ = self.critic.predict(state_)
         critic_value = self.critic.predict(state)
 
@@ -61,6 +72,35 @@ class Agent(object):
         actions = np.zeros([1, self.n_actions])
         actions[np.arange(1), action] = 1
 
-        self.actor.fit([state, delta], actions, verbose=0)
+        self.actor.fit([state, tau, delta], actions, verbose=0)
 
         self.critic.fit(state, target, verbose=0)
+        
+    def learn_batches(self, mb_size, state, action, reward, state_, done, tau=1.0, shuffle=True):
+        # make sure data is np arrays
+        state = np.array(state)
+        tau = np.ones((len(state),))*tau
+        action = np.array(action)
+        reward = np.array(reward)
+        state_ = np.array(state_)
+        done = np.array(done, dtype=np.int)
+        #print("learn_batches: inputs:", state.shape, action.shape, reward.shape, state_.shape, done.shape)
+        
+        n = len(state)
+        
+        critic_value = self.critic.predict(state)[:,0]
+        critic_value_ = self.critic.predict(state_)[:,0]
+
+        target = reward + self.gamma*critic_value_*(1-done)
+        delta =  target - critic_value
+        #print("learn_batches: delta:", delta.shape, delta)
+
+        actions = np.zeros([n, self.n_actions])
+        actions[np.arange(n), action] = 1
+
+        actor_metrics = self.actor.fit([state, tau, delta], actions, batch_size=mb_size, verbose=0, shuffle=shuffle)
+
+        critic_metrics = self.critic.fit(state, target, batch_size=mb_size, verbose=0, shuffle=shuffle)
+        
+        return actor_metrics, critic_metrics
+        
--- AC-Phil/train.py
+++ AC-Phil/train.py
@@ -7,18 +7,91 @@ env = gym.make("LunarLander-v2")
 score_history = []
 num_episodes = 2000
 
-for t in range(num_episodes):
-    done = False
-    observation = env.reset()
-    score = 0.0
+class Trainer(object):
     
-    while not done:
-        action = agent.choose_action(observation)
-        observation_, reward, done, info = env.step(action)
-        agent.learn(observation, action, reward, observation_, done)
-        observation = observation_
-        score += reward
+    def __init__(self, agent, env, mb_size = 50, train_interval = 10, shuffle = True):
+        self.Agent = agent
+        self.Env = env
+        self.BatchSize = mb_size
+        self.TrainInterval = train_interval
+        self.Shuffle = shuffle
+
+    def run_episode(self):
+        done = False
+        observation = self.Env.reset()
+        score = 0.0
+        record = []
+        while not done:
+            action = self.Agent.choose_action(observation)
+            observation_, reward, done, info = self.Env.step(action)
+            record.append((observation, action, reward, observation_, 1.0 if done else 0.0, info))
+            observation = observation_
+            score += reward
+        return score, record
+        
+    def train(self, num_episodes, tau=1.0):
+        history = []
+        next_train = self.TrainInterval
+        num_trained = 0
+        score_history = []
+        actor_metrics, critic_metrics = None, None
+        for t in range(num_episodes):
+            score, record = self.run_episode()
+            score_history.append(score)
+            history += record
+            t += 1
+            if t >= next_train:
+                next_train += self.TrainInterval
+                x0, a, r, x1, f, infos = zip(*history)
+                x0 = np.array(x0)
+                a = np.array(a)
+                r = np.array(r)
+                x1 = np.array(x1)
+                f = np.array(f)
+                actor_metrics, critic_metrics = self.Agent.learn_batches(self.BatchSize, x0, a, r, x1, f, tau=tau, shuffle=self.Shuffle)
+                num_trained += len(history)
+                history = []
+                yield t, actor_metrics, critic_metrics, score, np.mean(score_history[-100:])
+        yield t, actor_metrics, critic_metrics, score, np.mean(score_history[-100:])
+        
+class Tester(object):
+    def __init__(self, agent, env, tau=10.0):
+        self.Env = env
+        self.Agent = agent
+        self.Tau = tau
+        
+    def run_episode(self):
+        done = False
+        observation = self.Env.reset()
+        score = 0.0
+        record = []
+        while not done:
+            action = self.Agent.choose_action(observation, tau=self.Tau)
+            observation_, reward, done, info = self.Env.step(action)
+            record.append((observation, action, reward, observation_, 1.0 if done else 0.0, info))
+            observation = observation_
+            score += reward
+        return score, record
+        
+    def test(self, num_episodes=10):
+        sum_score = 0.0
+        for t in range(num_episodes):
+            score, record = self.run_episode()
+            sum_score += score
+        return sum_score/num_episodes
+            
     
-    score_history.append(score)
-    avg_score = np.mean(score_history[-100:])
-    print("Episode:", t, "  score:", score, "  average score:", avg_score)
+    
+                
+trainer = Trainer(agent, env, mb_size=30)
+tester = Tester(agent, env, tau=100.0)
+best_average = None
+best_average_t = t = 0
+for num_trained, actor_metrics, critic_metrics, score, avg_score in trainer.train(num_episodes, tau=0.2):
+    if best_average is None or best_average < avg_score:
+        best_average = avg_score
+        best_average_t = num_trained
+    test_score = tester.test()
+    print("Episodes: %6d   score: %+8.2f   average score: %+8.2f   best average: %+8.2f at %-6d --- test score: %+8.2f" % \
+            (num_trained, score, avg_score, best_average, best_average_t, test_score))
+                
